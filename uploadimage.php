409c6e225400a7a02fcc7687249316cadc165fb9
Takoni <89021450+Takoni@users.noreply.github.com>
2022-02-08 21:27:19 +0800

    commit

--- uploadimage.php
+++ uploadimage.php
@@ -38,17 +38,53 @@
 
 <html>
     <head>
+        <link rel="icon" type="image/png" sizes="16x16" href="photos/favicon.png">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
+        <link href="css/header-style.css?t=[timestamp]" type="text/css" rel="stylesheet">
+        <link href="css/footer-style.css?t=[timestamp]" type="text/css" rel="stylesheet">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>NEWS</title>
         <link rel="stylesheet"href="css/news-style.css"type="text/css">
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     </head>
     <body>
         
-    <div class="topic">
-        <h1>WILDVENGER MALAYSIA</h1>
-        <h2 class="aaa">NEWS IN MALAYSIA</h2>
-    </div>
+    <div class = "topnav" style = "font-family: 'Roboto Mono', monospace" >
+        <div class = "top">
+          <div class = "top-left">
+            <div class = "top-left-me ">
+              <a href="index.php">HOME</a>
+            </div>
+            <div class = "top-left-me ">
+              <a href="login.php">LOGIN</a>
+            </div>
+            <div class = "top-left-me ">
+              <a href="organization.php">ORGANIZATIONS</a>
+            </div>
+            <div class = "top-left-me ">
+              <a href="explore.php">EXPLORE</a>
+            </div>
+            <div class = "top-left-me front">
+              <a href="news.php">NEWS</a>
+            </div>
+            <div class = "top-left-me ">
+              <a href="forum.php">FORUM</a>
+            </div>
+            </div>
+          <div class = "top-right">
+            <div class = "top-right-me">
+              Wildvenger Malaysia
+            </div>
+          </div>
+        </div>
+      </div>
     <div class="onoffswitch3">
         <input type="checkbox" name="onoffswitch3" class="onoffswitch3-checkbox" id="myonoffswitch3" checked>
         <label class="onoffswitch3-label" for="myonoffswitch3">
@@ -60,9 +96,10 @@
                 <span class="onoffswitch3-inactive"><span class="onoffswitch3-switch">SHOW BREAKING NEWS</span></span>
             </span>
         </label>
+        <p></p>
         <div class="container">
         <form method="POST" enctype="multipart/form-data">
-            <input type="hidden" name="size" value="1000000">
+            <input type="hidden" name="size" value="1000000" >
                 Image: <input type="file" name="image"><br>
                 Title: <input type="text" placeholder= "Title"name="title"><br>
                  Description: <input type="text" placeholder= "Description" name="descp"><br>
